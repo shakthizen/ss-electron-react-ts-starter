e42a38704f141aad59e200ab8a1f90e33c2612eb
Shakthi Senavirathna <shakthis.mailbox@gmail.com>
2021-08-07 22:18:31 +0530

    Changed main.ts to electron.ts, Changed render.js to index.tsx, Changed App.tsx, Added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from "react";
 
-function render() {
-  ReactDOM.render(<h2>Hello React !</h2>, document.body);
+export default function App(): JSX.Element {
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection:"column"
+      }}
+    >
+      <div style={{fontSize:"32pt",fontWeight:"bold",color:"#333333"}}>Hello !</div>
+      <div style={{fontSize:"18pt",fontWeight:"lighter",color:"#333333"}}>From React</div>
+    </div>
+  );
 }
-
-render();
